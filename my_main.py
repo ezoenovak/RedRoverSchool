f4eb67e19cc24f39923944a07f933923478084fa
Zoe Novak <129424393+ezoenovak@users.noreply.github.com>
2023-06-08 17:58:48 +0400

    feat: add one test

---
+++ my_main.py
@@ -0,0 +1,32 @@
+from lib2to3.pgen2 import driver
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+import string
+import random
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+
+from selenium.webdriver.support.select import Select
+
+
+def test_waits_book():
+    driver = webdriver.Chrome()
+    driver.get("http://suninjuly.github.io/explicit_wait2.html")
+    WebDriverWait(driver, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), "100"))
+    driver.find_element(By.ID, "book").click()
+    num = driver.find_element(By.ID, "input_value")
+    x = int(num.text)
+
+    def calc(x):
+        return str(math.log(abs(12 * math.sin(int(x)))))
+
+    y = calc(x)
+    driver.find_element(By.ID, "answer").send_keys(y)
+    driver.find_element(By.ID, "solve").click()
+    time.sleep(15)
+    driver.quit()
+
